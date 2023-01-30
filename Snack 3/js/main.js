2b1233762fd8eabbb0ee2b2e30151cf4ada678cf
Simone Marino <simo.marino@outlook.it>
2023-01-30 21:00:14 +0100

    isNan check

--- Snack 3/js/main.js
+++ Snack 3/js/main.js
@@ -3,13 +3,6 @@
 // Genera N array, ognuno formato da 10 numeri casuali da 1 a 100.
 // Ogni volta che ne crei uno, stampalo.
 
-// emptyList = [];
-// while (emptyList.length < 10) {
-
-// }
-
-// newArray(N);
-// console.log(newArray());
 const N = parseInt(prompt("Scegli il numero di arrays da creare"));
 
 let i = 0;
@@ -19,4 +12,17 @@ while (i < N) {
     .map(() => Math.floor(100 * Math.random()));
   console.log(lista);
   i++;
+}
+
+if (isNaN(N)) {
+  alert("attenzione, inserisci un numero per favore.");
+  const N = parseInt(prompt("Scegli il numero di arrays da creare"));
+  let i = 0;
+  while (i < N) {
+    const lista = Array(10)
+      .fill()
+      .map(() => Math.floor(100 * Math.random()));
+    console.log(lista);
+    i++;
+  }
 }
